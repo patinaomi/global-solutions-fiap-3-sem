acbef1651dbd3f708d744df4ead5d72ed0ab5798
Patricia Naomi <patricianaomiyama@gmail.com>
2024-08-08 20:12:06 -0300

    teste

--- aula1.sql
+++ aula1.sql
@@ -4,8 +4,6 @@ BEGIN
             table_name
         FROM
             user_tables
-        WHERE
-            table_name = 'PEDIDO_NOVO'
     ) LOOP
         EXECUTE IMMEDIATE 'DROP TABLE '
                           || x.table_name
