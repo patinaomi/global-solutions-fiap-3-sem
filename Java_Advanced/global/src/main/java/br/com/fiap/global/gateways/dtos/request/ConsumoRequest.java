27748267c74964de9ad3414a2e1bfad7701167a1
Patricia Naomi <patricianaomiyama@gmail.com>
2024-11-18 18:42:08 -0300

    FIX: Consumo Request

---
+++ Java_Advanced/global/src/main/java/br/com/fiap/global/gateways/dtos/request/ConsumoRequest.java
@@ -0,0 +1,16 @@
+package br.com.fiap.global.gateways.dtos.request;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+
+@Getter
+@Setter
+public class ConsumoRequest extends RepresentationModel<ConsumoRequest> {
+
+    @NotNull(message = "O campo descrição é obrigatório")
+    @Size(message = "O campo descrição deve ter entre 3 e 50 caracteres", min = 3, max = 50)
+    private String descricao;
+}
